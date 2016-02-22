45f8c7168829f3ddf868661aa14010b0f6dfa151
David Tang <dtang859@gmail.com>
2016-02-21 17:13:14 -0800

    working

--- app/serializers/user.js
+++ app/serializers/user.js
@@ -1,16 +1,21 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
-  normalizeFindAllResponse(store, primaryModelClass, payload, id, requestType) {
-    payload.users.map((user) => {
-      user.links = {
-        pets: `/api/v1/users/${user.id}/pets`,
-        company: `/api/v1/users/${user.id}/company`
-      };
-    });
-
-    console.log(payload);
-
-    return this._super(...arguments);
+  urlForFindHasMany (id, modelName) {
+    if (modelName === 'pets') {
+      return `/api/v1/users/${id}/pets`;
+    }
   }
+  // normalizeFindAllResponse(store, primaryModelClass, payload, id, requestType) {
+  //   payload.users.map((user) => {
+  //     user.links = {
+  //       pets: `/api/v1/users/${user.id}/petsfff`,
+  //       company: `/api/v1/users/${user.id}/companyffff`
+  //     };
+  //   });
+  //
+  //   console.log(payload);
+  //
+  //   return this._super(...arguments);
+  // }
 });
